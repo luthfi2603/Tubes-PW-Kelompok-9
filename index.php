2b12536b8d070e525e54f4271235bc08780b4167
luthfi2603 <luthfim904@gmail.com>
2023-01-04 09:59:58 +0700

    push ke github

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KyuuDent_Store</title>
-    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/img/favicon2.PNG" type="image/x-icon">
     <style>
         <?php
             // memasukkan custom css
